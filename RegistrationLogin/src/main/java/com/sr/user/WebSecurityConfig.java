897aba87792ecaee8d1cbb987cbf33833cc9e28c
musrihar <musrihar@LIN76001839.corp.capgemini.com>
2021-04-06 11:27:08 +0530

    Final Code Uploaded with h2 database

--- RegistrationLogin/src/main/java/com/sr/user/WebSecurityConfig.java
+++ RegistrationLogin/src/main/java/com/sr/user/WebSecurityConfig.java
@@ -47,12 +47,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-        .antMatchers("/users").authenticated()
+        .antMatchers("/list_users").authenticated()
         .anyRequest().permitAll()
         .and()
         .formLogin()
             .usernameParameter("email")
-            .defaultSuccessUrl("/users")
+            .defaultSuccessUrl("/list_users")
             .permitAll()
         .and()
         .logout().logoutSuccessUrl("/").permitAll();
